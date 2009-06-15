2f81f43afaf844f2fab2ecd5f83ffb9d56580b6c
mats cronqvist <mats.cronqvist@gmail.com>
2009-06-15 12:49:39 +0000

    bug in module check (disallowed preloaded)
    git-svn-id: https://eper.googlecode.com/svn/trunk@204 5f96f306-4121-0410-aeb0-fb9935354dc2

--- src/prfTrc.erl
+++ src/prfTrc.erl
@@ -130,11 +130,14 @@ maybe_load_modules(Conf) ->
 
 maybe_load_rtp({{M,F,A},_MatchSpec,_Flags} = Rtp,O) ->
   try 
-    "/" = [hd(code:which(M))],
-    [c:l(M) || false == code:is_loaded(M)],
+    case code:which(M) of
+      non_existing      -> throw(non_existing_module);
+      preloaded         -> ok;
+      F when is_list(F) -> [c:l(M) || false == code:is_loaded(M)]
+    end,
     [Rtp|O]
   catch 
-    error:R -> ?log({no_such_function,{R,{M,F,A}}}), O
+    _:R -> ?log({no_such_function,{R,{M,F,A}}}), O
   end.
 
 family(Daddy) ->
