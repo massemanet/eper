29edce5290950cc842ea127aaee0015a0c78069d
mats cronqvist <masse@klarna.com>
2012-06-15 15:41:53 +0000

    dtop_replay did not support options 'time', 'delta_time', 'start_time' and 'stop_time'.

--- src/dtop_replay.erl
+++ src/dtop_replay.erl
@@ -13,9 +13,9 @@
 
 replay(TrcFile,Node) -> replay(TrcFile,Node,[]).
 
-replay(TrcFile,OldNode,Opts) ->
+replay(File,OldNode,Opts) ->
   Node = ck_node(OldNode),
-  replay_trc:go(TrcFile,mk_dtop_fun(Node),mk_dtop_init(Node,Opts),Opts).
+  replay_trc:go(File,mk_dtop_fun(),mk_dtop_init(Node,Opts),[{node,Node}|Opts]).
 
 ck_node(Node) ->
   case string:tokens(atom_to_list(Node),"@") of
@@ -23,10 +23,10 @@ ck_node(Node) ->
     _     -> exit({not_a_node,Node})
   end.
 
-mk_dtop_fun(Node) ->
-  fun(done,{C,_})                                   -> {items,C};
-     ({watchdog,N,_,ticker,D},{C,LD}) when N=:=Node -> {C+1,tick(LD,D)};
-     (_,{C,LD})                                     -> {C,LD}
+mk_dtop_fun() ->
+  fun(done,{C,_})                     -> {items,C};
+     ({watchdog,_,_,ticker,D},{C,LD}) -> {C+1,tick(LD,D)};
+     (_,{C,LD})                       -> {C,LD}
   end.
 
 mk_dtop_init(Node,Opts) ->
