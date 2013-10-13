93654422e86d7bddf1d33450aaca2e6191f75a10
Susan Potter <me@susanpotter.net>
2013-10-13 16:20:43 -0500

    Remove crypto module deprecations
    When compiling with R16B02 the following deprecation warnings were
    encountered:
    
    ```
    src/prf_crypto.erl:36: Warning: crypto:des_cbc_encrypt/3 is deprecated and will be removed in in a future release; use crypto:block_encrypt/4
    src/prf_crypto.erl:43: Warning: crypto:des_cbc_decrypt/3 is deprecated and will be removed in in a future release; use crypto:block_decrypt/4
    src/prf_crypto.erl:46: Warning: crypto:md5/1 is deprecated and will be removed in in a future release; use crypto:hash/2
    ```
    
    Since I use `warnings_as_errors` erlc option I needed to work around these
    deprecation warnings for newer versions of Erlang (R16B+). I did this using
    a `rebar.config.script` and the Erlang preprocessor's `-ifdef` construct.
    Therefore, it should work for previous versions as well as newer versions of
    Erlang.
    
    This does make one assumption, an assumption I believe to be "safe", but
    worth noting anyway. It assumes that if `crypto:block_encrypt/4` is exported
    from the `crypto` module that `crypto:block_decrypt/4` will also be exported.
    
    Happy to amend that shortcut if necessary.

--- src/prf_crypto.erl
+++ src/prf_crypto.erl
@@ -8,6 +8,24 @@
 -author('Mats Cronqvist').
 -export([encrypt/1,encrypt/2,decrypt/1,decrypt/2]).
 
+-ifdef(USE_CRYPTO_HASH).
+-define(CRYPTO_HASH(Data), crypto:hash(md5, Data)).
+-else.
+-define(CRYPTO_HASH(Data), crypto:md5(Data)).
+-endif.
+
+-ifdef(USE_CRYPTO_BLOCK_CRYPT).
+-define(CRYPTO_BLOCK_ENCRYPT(Key, IVec, Text),
+  crypto:block_encrypt(des_cbc, Key, IVec, Text)).
+-define(CRYPTO_BLOCK_DECRYPT(Key, IVec, Text),
+  crypto:block_decrypt(des_cbc, Key, IVec, Text)).
+-else.
+-define(CRYPTO_BLOCK_ENCRYPT(Key, IVec, Text),
+  crypto:des_cbc_encrypt(Key, IVec, Text)).
+-define(CRYPTO_BLOCK_DECRYPT(Key, IVec, Text),
+  crypto:des_cbc_decrypt(Key, IVec, Text)).
+-endif.
+
 phrase() -> atom_to_list(erlang:get_cookie()).
 
 encrypt(Data) -> encrypt(phrase(),Data).
@@ -15,17 +33,17 @@ encrypt(Data) -> encrypt(phrase(),Data).
 encrypt(Phrase,Data) ->
   assert_crypto(),
   {Key,Ivec} = make_key(Phrase),
-  crypto:des_cbc_encrypt(Key,Ivec,pad(Data)).
+  ?CRYPTO_BLOCK_ENCRYPT(Key, Ivec, pad(Data)).
 
 decrypt(Data) -> decrypt(phrase(),Data).
 
 decrypt(Phrase,Data) ->
   assert_crypto(),
   {Key,Ivec} = make_key(Phrase),
-  unpad(crypto:des_cbc_decrypt(Key,Ivec,Data)).
+  unpad(?CRYPTO_BLOCK_DECRYPT(Key, Ivec, Data)).
 
 make_key(Phrase) ->
-  <<Key:8/binary,Ivec:8/binary>> = crypto:md5(Phrase),
+  <<Key:8/binary,Ivec:8/binary>> = ?CRYPTO_HASH(Phrase),
   {Key,Ivec}.
 
 pad(Term) ->
