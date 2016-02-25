a74a5da7c9d41ea50a8f7f7c94d5526c5257b52d
mats cronqvist <masse@cronqvi.st>
2016-02-25 19:53:53 +0100

    redo hiding of deprecated funcs in prf_crypto

--- src/prf_crypto.erl
+++ src/prf_crypto.erl
@@ -8,14 +8,14 @@
 -author('Mats Cronqvist').
 -export([encrypt/1,encrypt/2,decrypt/1,decrypt/2]).
 
--ifdef(CRYPTO_R16).
--define(HASH(Data),hash(md5,Data)).
--define(BLOCK_ENCRYPT(Key,IVec,Text),block_encrypt(des_cbc,Key,IVec,Text)).
--define(BLOCK_DECRYPT(Key,IVec,Text),block_decrypt(des_cbc,Key,IVec,Text)).
+-ifdef(USE_PRE_R16_CRYPTO).
+hash(Data) -> crypto:md5(Data).
+block_encrypt(Key,IVec,Text) -> crypto:des_cbc_encrypt(Key,IVec,Text).
+block_decrypt(Key,IVec,Text) -> crypto:des_cbc_decrypt(Key,IVec,Text).
 -else.
--define(HASH(Data),md5(Data)).
--define(BLOCK_ENCRYPT(Key,IVec,Text),des_cbc_encrypt(Key,IVec,Text)).
--define(BLOCK_DECRYPT(Key,IVec,Text),des_cbc_decrypt(Key,IVec,Text)).
+hash(Data) -> crypto:hash(md5,Data).
+block_encrypt(Key,IVec,Text) -> crypto:block_encrypt(des_cbc,Key,IVec,Text).
+block_decrypt(Key,IVec,Text) -> crypto:block_decrypt(des_cbc,Key,IVec,Text).
 -endif.
 
 phrase() -> atom_to_list(erlang:get_cookie()).
@@ -25,17 +25,17 @@ encrypt(Data) -> encrypt(phrase(),Data).
 encrypt(Phrase,Data) ->
   assert_crypto(),
   {Key,Ivec} = make_key(Phrase),
-  crypto:?BLOCK_ENCRYPT(Key,Ivec,pad(Data)).
+  block_encrypt(Key,Ivec,pad(Data)).
 
 decrypt(Data) -> decrypt(phrase(),Data).
 
 decrypt(Phrase,Data) ->
   assert_crypto(),
   {Key,Ivec} = make_key(Phrase),
-  unpad(crypto:?BLOCK_DECRYPT(Key,Ivec,Data)).
+  unpad(block_decrypt(Key,Ivec,Data)).
 
 make_key(Phrase) ->
-  <<Key:8/binary,Ivec:8/binary>> = crypto:?HASH(Phrase),
+  <<Key:8/binary,Ivec:8/binary>> = hash(Phrase),
   {Key,Ivec}.
 
 pad(Term) ->
