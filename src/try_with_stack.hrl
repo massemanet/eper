054f2cde5fe225430be42d718df7f5ee5ba7fcec
mats cronqvist <masse@cronqvi.st>
2018-07-06 10:58:20 +0200

    fix for OTP16

--- src/try_with_stack.hrl
+++ src/try_with_stack.hrl
@@ -3,10 +3,10 @@
 %% work around the deprecation of erlang:get_stacktrace/0 in erlang 21
 %% @end
 
--ifdef('USE_GET_STACKTRACE').
--define(try_with_stack(F),
-        try {ok,F} catch __C:__R -> {__C,__R,erlang:get_stacktrace()} end).
--else.
+-ifdef('OTP_RELEASE'). %% implies > OTP21
 -define(try_with_stack(F),
         try {ok,F} catch __C:__R:__S -> {__C,__R,__S} end).
+-else.
+-define(try_with_stack(F),
+        try {ok,F} catch __C:__R -> {__C,__R,erlang:get_stacktrace()} end).
 -endif.
